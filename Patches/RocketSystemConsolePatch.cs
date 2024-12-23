16a84339a4fb4e38363897d916be49aaaa34476f
pixelblob <phillipsfamily.ethan@gmail.com>
2024-12-24 00:58:32 +0200

    Cleaned up structure of code

---
+++ Patches/RocketSystemConsolePatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UI.ImGuiUi;
+
+namespace ConsolePatch.Patches
+{
+    [HarmonyPatch(typeof(RocketSystemConsole))]
+    internal class RocketSystemConsolePatch
+    {
+        [HarmonyPatch("SetConsoleOutputCP")]
+        [HarmonyPrefix]
+        static bool SetConsoleOutputCP_Patch()
+        {
+            return false;
+        }
+
+        [HarmonyPatch("SetConsoleCP")]
+        [HarmonyPrefix]
+        static bool SetConsoleCP_Patch()
+        {
+            return false;
+        }
+
+
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,55 +1,23 @@
-﻿using System;
-using System.IO;
-using System.Collections;
-using System.Collections.Generic;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using UnityEngine;
-using System.Reflection;
-using UI.ImGuiUi;
+using ConsolePatch.Patches;
 
-namespace MyFirstPlugin;
+namespace ConsolePatch;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-        
+    Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 
-        Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
-
-        MethodInfo original = AccessTools.Method(typeof(RocketSystemConsole), "SetConsoleOutputCP");
-
-        MethodInfo patch = AccessTools.Method(typeof(MyPatches), "SetConsoleOutputCP_MyPatch");
-
-        harmony.Patch(original, new HarmonyMethod(patch));
-
-        MethodInfo original1 = AccessTools.Method(typeof(RocketSystemConsole), "SetConsoleCP");
-
-        MethodInfo patch1 = AccessTools.Method(typeof(MyPatches), "SetConsoleCP_MyPatch");
-
-        harmony.Patch(original1, new HarmonyMethod(patch1));
-
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loadedaaaaaaaaa!");
-    }
-}
-
-
-public class MyPatches
-{
-    public static bool SetConsoleOutputCP_MyPatch(uint wCodePageID)
-    {
-        return false;
-    }
-
-    public static bool SetConsoleCP_MyPatch(uint wCodePageID)
-    {
-        return false;
+        harmony.PatchAll(typeof(Plugin));
+        harmony.PatchAll(typeof(RocketSystemConsolePatch));
     }
 }
