667cb03615a6cf8e1d389a96d3cad367ee076c01
pixelblob <phillipsfamily.ethan@gmail.com>
2024-12-24 12:04:25 +0200

    saveonexit

---
+++ Patches/GridManagerPatch.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts.GridSystem;
+using Assets.Scripts;
+using HarmonyLib;
+using System;
+using Assets.Scripts.Serialization;
+
+namespace ConsolePatch.Patches
+{
+    [HarmonyPatch(typeof(GridManager))]
+    internal class GridManagerPatch
+    {
+        [HarmonyPatch("OnApplicationQuit")]
+        [HarmonyPrefix]
+        static bool OnApplicationQuit_Patch()
+        {
+            Console.WriteLine("--------------------- GridManager.OnApplicationQuit ---------------------");
+
+            Console.WriteLine(!GameManager.IsNewTutorial);
+            Console.WriteLine(!GameManager.IsTutorial);
+
+            GameState gameState = GameManager.GameState;
+
+            Console.WriteLine(gameState != GameState.None);
+            Console.WriteLine(gameState != GameState.Loading);
+            Console.WriteLine(gameState);
+            Console.WriteLine(XmlSaveLoad.Instance.SavingWorld);
+
+            Console.WriteLine("SAVING");
+
+            Plugin.Instance.SaveCurrent();
+
+            Console.WriteLine("DONE");
+            GameManager.GameState = GameState.None;
+            //Application.Quit();
+            return false;
+        }
+
+
+
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -2,6 +2,14 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using ConsolePatch.Patches;
+using UnityEngine;
+using System;
+using Assets.Scripts.Serialization;
+using System.Reflection;
+using System.Threading;
+using Assets.Scripts.GridSystem;
+using Assets.Scripts;
+using System.IO;
 
 namespace ConsolePatch;
 
@@ -11,13 +19,31 @@ public class Plugin : BaseUnityPlugin
     internal static new ManualLogSource Logger;
     Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
 
+    public static Plugin Instance;
+
     private void Awake()
     {
+        Plugin.Instance = this;
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 
         harmony.PatchAll(typeof(Plugin));
         harmony.PatchAll(typeof(RocketSystemConsolePatch));
+        harmony.PatchAll(typeof(GridManagerPatch));
+
+    }
+
+    public void SaveCurrent()
+    {
+        MethodInfo writeWorld = typeof(XmlSaveLoad).GetMethod("WriteWorld", BindingFlags.NonPublic | BindingFlags.Instance);
+        FieldInfo _validWordData = typeof(XmlSaveLoad).GetField("_validWordData", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        DirectoryInfo saveDir = XmlSaveLoad.Instance.CurrentWorldSave.Directory();
+        Console.WriteLine(saveDir.ToString());
+
+        _validWordData.SetValue(XmlSaveLoad.Instance, true);
+
+        writeWorld.Invoke(XmlSaveLoad.Instance, [saveDir.ToString(), false, false]);
     }
 }
