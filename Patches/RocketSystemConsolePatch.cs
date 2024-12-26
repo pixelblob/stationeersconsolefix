7d6cd14f7090847df65f20335f8260c221c44fd8
pixelblob <phillipsfamily.ethan@gmail.com>
2024-12-26 07:01:58 +0200

    Added proper stdin handling

--- Patches/RocketSystemConsolePatch.cs
+++ Patches/RocketSystemConsolePatch.cs
@@ -1,5 +1,14 @@
-﻿using HarmonyLib;
+﻿using Assets.Scripts;
+using Assets.Scripts.Objects.Items;
+using Assets.Scripts.Serialization;
+using DLC;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
 using UI.ImGuiUi;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsolePatch.Patches
 {
@@ -17,6 +26,49 @@ namespace ConsolePatch.Patches
         [HarmonyPrefix]
         static bool SetConsoleCP_Patch()
         {
+            return false;
+        }
+
+        [HarmonyPatch("ConsoleInputThread")]
+        [HarmonyPrefix]
+        static bool ConsoleInputThread_Patch()
+        {
+            var RSCType = typeof(RocketSystemConsole);
+            var CWType = typeof(ConsoleWindow);
+            var bindingAttr = BindingFlags.NonPublic | BindingFlags.Instance;
+
+            var _systemConsoleInput = (RocketSystemConsole)CWType.GetField("_systemConsoleInput", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+
+            var _inputString = (StringBuilder)RSCType.GetField("_inputString", bindingAttr).GetValue(_systemConsoleInput);
+            var _inputPrefix = (string)RSCType.GetField("_inputPrefix", bindingAttr).GetValue(_systemConsoleInput);
+            var _keepAlive = (bool)RSCType.GetField("_keepAlive", bindingAttr).GetValue(_systemConsoleInput);
+
+            var RedrawInputLine = RSCType.GetMethod("RedrawInputLine", bindingAttr);
+            var OnEscape = RSCType.GetMethod("OnEscape", bindingAttr);
+            var OnEnter = RSCType.GetMethod("OnEnter", bindingAttr);
+            var OnBackspace = RSCType.GetMethod("OnBackspace", bindingAttr);
+
+            _inputString.Append(_inputPrefix);
+            while (_keepAlive)
+            {
+                if (Console.CursorVisible && Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                    ConsoleKey key = consoleKeyInfo.Key;
+                    if (key == ConsoleKey.Backspace) OnBackspace.Invoke(_systemConsoleInput, []);
+                    else if (key == ConsoleKey.Enter) OnEnter.Invoke(_systemConsoleInput, []);
+                    else if (key == ConsoleKey.Escape) OnEscape.Invoke(_systemConsoleInput, []);
+                    else if (consoleKeyInfo.KeyChar == '\0') RedrawInputLine.Invoke(_systemConsoleInput, []);
+                    else _inputString.Append(consoleKeyInfo.KeyChar);
+                }
+                else
+                {
+                    string text = Console.ReadLine();
+                    _inputString.Clear();
+                    _inputString.Append(text);
+                    OnEnter.Invoke(_systemConsoleInput, []);
+                }
+            }
             return false;
         }
 
